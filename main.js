bba842ebf06c6fec8fc8aac24c7ca6e029780c87
Sankhadeep Ghosh <144552017+sankhadeep-ghosh@users.noreply.github.com>
2024-09-15 14:23:18 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed('#element', {
+    strings: ['Web Developer', 'Graphic Designer','Cyber Security Specialist','Ethical Hacker'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+  });
